beccbdc439c66349fd02733c3a2e7d3cf46cc4ce
Hailang <thehailang@gmail.com>
2019-12-21 12:11:06 +0800

    Auto commit: 2019-12-21_12:11:06

--- src/email/mailgun/email.test.ts
+++ src/email/mailgun/email.test.ts
@@ -4,7 +4,7 @@ it('should send mail', async (done) => {
   const e = process.env
 
   const email = new Email({ domain: e.domain, apiKey: e.key })
-  const p = email.send('support@message.bsudo.com', 'thehailang@gmail.com', 'Yo~', 'Test <strong>mail</strong>')
+  const p = email.send(e.from, e.to, 'Yo~', 'Test <strong>mail</strong>')
   p.catch(e => {
     done()
   })
